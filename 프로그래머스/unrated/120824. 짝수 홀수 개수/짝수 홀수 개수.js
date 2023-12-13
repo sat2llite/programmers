706faf51ab4d152c86ffa71ed4b9470b6db5550c
Seung Yeon Baek <63222786+sat2llite@users.noreply.github.com>
2023-12-13 15:28:58 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,17 +1,21 @@
 function solution(num_list) {
-    let arr = num_list.map((el) => {
-        if(el % 2 == 0) { 
-            // return arr.push(el.length);
-            return "짝수";
-        }
-        else { 
-            return "홀수"; 
-        }
-        for (let i=0; i<=el.length; i++) {
-            return console.log("d");
-        }
-    });
-    let a = arr.filter(item => "짝수" === item).length;
-    let b = arr.filter(item => "홀수" === item).length;
+    // let arr = num_list.map((el) => {
+    //     if(el % 2 == 0) { 
+    //         // return arr.push(el.length);
+    //         return "짝수";
+    //     }
+    //     else { 
+    //         return "홀수"; 
+    //     }
+    //     for (let i=0; i<=el.length; i++) {
+    //         return console.log("d");
+    //     }
+    // });
+    // let a = arr.filter(item => "짝수" === item).length;
+    // let b = arr.filter(item => "홀수" === item).length;
+    // return [a, b];
+    
+    let a = num_list.filter(item =>  item%2 === 0).length;
+    let b = num_list.filter(item => (item%2)-1 === 0).length;
     return [a, b];
 }
