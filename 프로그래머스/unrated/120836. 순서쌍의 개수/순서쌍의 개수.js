0c1bd1be9ccc9bd8ffa7cda9b476eadede1bf364
Seung Yeon Baek <63222786+sat2llite@users.noreply.github.com>
2023-12-19 17:37:53 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 5.65 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let arr = [];
+    
+    for (let i = 0; i <= n; i++) {
+        if (n % i == 0) {
+            arr.push(i);
+        }
+    }
+    return arr.length;
+}
