d3cd80b9cc4550a1669e14078d1aebc68c385e9f
Seung Yeon Baek <63222786+sat2llite@users.noreply.github.com>
2023-12-19 14:01:01 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,8 +1,6 @@
 function solution(sides) {
-    let result;
     sides.sort((a, b) => a - b);
     if (sides[0] + sides[1] <= sides[2]) {
         return 2;
     } else return 1;
-    return sides;
 }
