00a530c8e6122b3884e329a39a046dde1fc17548
Seung Yeon Baek <63222786+sat2llite@users.noreply.github.com>
2024-01-04 17:17:27 +0900

    [level unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,10 @@
+function solution(my_string, index_list) {
+    let arr = [...my_string];
+    let result = [];
+    
+    for (let i = 0; i < index_list.length; i++) {
+        result.push(my_string[index_list[i]]);
+    }
+    
+    return result.join("");
+}
